bbf29fb9cc862429e269ba7b177208589d917e41
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-07-27 11:14:00 +1200

    PSR-2

--- src/Tasks/SearchEngineBaseTask.php
+++ src/Tasks/SearchEngineBaseTask.php
@@ -116,48 +116,29 @@ class SearchEngineBaseTask extends BuildTask
 
         $this->flushNow('<h2>Starting</h2>', false);
 
-        $verbose = intval($request->getVar('verbose'));
-        if ($verbose) {
-            $this->verbose = $verbose;
-        }
+        $this->verbose = intval($request->getVar('verbose'));
         $this->flushNow('<strong>verbose</strong>: ' . ($this->verbose ? 'yes' : 'no'));
 
-        $limit = intval($request->getVar('limit'));
-        if ($limit) {
-            $this->limit = $limit;
+        if($request->getVar('limit')) {
+            $this->limit = intval($request->getVar('limit'));
         }
         $this->flushNow('<strong>limit</strong>: ' . $this->limit);
 
-        $step = intval($request->getVar('step'));
-        if ($step) {
-            $this->step = $step;
+        if($request->getVar('step')) {
+            $this->step = intval($request->getVar('step'));
         }
         $this->flushNow('<strong>step</strong>: ' . $this->step);
 
-        $type = $request->getVar('type');
-        if ($type) {
-            $this->type = $type;
-        }
+        $this->type = $request->getVar('type');
         $this->flushNow('<strong>type</strong>: ' . $this->type);
 
-        $oldOnesOnly = $request->getVar('oldonesonly') ? true : false;
-        if ($oldOnesOnly) {
-            $this->oldOnesOnly = $oldOnesOnly;
-        }
+        $this->oldOnesOnly = $request->getVar('oldonesonly') ? true : false;
         $this->flushNow('<strong>old ones only</strong>: ' . ($this->oldOnesOnly ? 'yes' : 'no'));
 
-        $unindexedOnly = $request->getVar('unindexedonly') ? true : false;
-        if ($unindexedOnly) {
-            $this->unindexedOnly = $unindexedOnly;
-        }
+        $this->unindexedOnly = $request->getVar('unindexedonly') ? true : false;
         $this->flushNow('<strong>unindexed only</strong>: ' . ($this->unindexedOnly ? 'yes' : 'no'));
 
-        $task = $request->getVar('task');
-        if ($task) {
-            $this->task = $task;
-        } else {
-            $this->task = self::$segment;
-        }
+        $this->task = $request->getVar('task') ? : self::$segment;
         $this->flushNow('<strong>task</strong>: ' . $this->task);
 
         $this->flushNow('==========================', false);
