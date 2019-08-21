305d5781a6d16fff449f82e6d569e715ca0be65f
thiemen <nt@sunnysideup.co.nz>
2019-08-22 11:23:18 +1200

    MINOR: better tasks

---
+++ src/Tasks/SearchEngineRemovePagesToBeUpdated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sunnysideup\SearchSimpleSmart\Tasks;
+
+use SilverStripe\Core\Config\Config;
+use SilverStripe\ORM\DB;
+
+class SearchEngineRemovePagesToBeUpdated extends SearchEngineBaseTask
+{
+    /**
+     * Title of the task
+     * @var string
+     */
+    protected $title = 'Remove Entries that should be updated';
+
+    /**
+     * Description of the task
+     * @var string
+     */
+    protected $description = 'Goes through all objects marked as to be indexed and removes them from this list so that you just run a couple.';
+
+    /**
+     * Set a custom url segment (to follow dev/tasks/)
+     *
+     * @config
+     * @var string
+     */
+    private static $segment = 'searchengineremovetobeindexed';
+
+    /**
+     * this function runs the SearchEngineRemoveAll task
+     * @param HTTPRequest $request
+     */
+    public function run($request)
+    {
+        $this->runStart($request);
+        DB::query('DELETE FROM "SearchEngineDataObjectToBeIndexed" WHERE Completed = 0');
+        $this->runEnd($request);
+    }
+}
