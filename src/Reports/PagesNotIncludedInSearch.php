e7b965569bd7773c2a74e203255e4190d7e0b237
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-06-07 13:22:08 +1200

    MINOR: add report about pages not included

---
+++ src/Reports/PagesNotIncludedInSearch.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Sunnysideup\SearchSimpleSmart\Reports;
+
+use SilverStripe\CMS\Model\RedirectorPage;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\CMS\Model\VirtualPage;
+use SilverStripe\ORM\DataList;
+use SilverStripe\Reports\Report;
+
+class PagesNotIncludedInSearch extends Report
+{
+    public function title()
+    {
+        return 'Pages not included in seearch';
+    }
+
+    public function group()
+    {
+        return _t(__CLASS__.'.ContentGroupTitle', "Content reports");
+    }
+
+    public function sort()
+    {
+        return 100;
+    }
+
+    /**
+     * Gets the source records
+     *
+     * @param array $params
+     * @return DataList
+     */
+    public function sourceRecords($params = null)
+    {
+        return SiteTree::get()
+            ->exclude(['ClassName' => RedirectorPage::class, VirtualPage::class])
+            ->filter(['ShowInSearch' => false])
+            ->sort('Title');
+    }
+
+    public function columns()
+    {
+        return [
+            "Title" => [
+                "title" => "Title", // todo: use NestedTitle(2)
+                "link" => true,
+            ],
+        ];
+    }
+}
